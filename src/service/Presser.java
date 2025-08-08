9c164359df880191135c70e6f92b7a267db4ac9d
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-08-08 23:03:50 +0800

    fix: code to be dynamic

--- src/service/Presser.java
+++ src/service/Presser.java
@@ -4,30 +4,44 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class Presser {
-    public void autoKey(){
+    public static void autoKey(String key1, String key2, String key3,
+                               String key4, String key5, int sleepTimeAfterOneKey, int sleepTimeAfterAllKey) {
         try {
             Robot robot = new Robot();
 
             while (true) {
-                robot.keyPress(KeyEvent.VK_3);
-                robot.keyRelease(KeyEvent.VK_3);
-                Thread.sleep(500); // .5s
+                pressKey(robot, key1);
+                Thread.sleep(sleepTimeAfterOneKey);
 
-                robot.keyPress(KeyEvent.VK_2);
-                robot.keyRelease(KeyEvent.VK_2);
-                Thread.sleep(500);
+                pressKey(robot, key2);
+                Thread.sleep(sleepTimeAfterOneKey);
 
-                robot.keyPress(KeyEvent.VK_4);
-                robot.keyRelease(KeyEvent.VK_4);
-                Thread.sleep(500);
+                pressKey(robot, key3);
+                Thread.sleep(sleepTimeAfterOneKey);
 
-                robot.keyPress(KeyEvent.VK_1);
-                robot.keyRelease(KeyEvent.VK_1);
-                Thread.sleep(3000); // 3s
+                pressKey(robot, key4);
+                Thread.sleep(sleepTimeAfterOneKey);
+
+                pressKey(robot, key5);
+                Thread.sleep(sleepTimeAfterAllKey);
             }
 
         } catch (AWTException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void pressKey(Robot robot, String key) {
+        if (key == null || key.isEmpty()) return;
+
+        char c = key.charAt(0);
+        int keyCode = KeyEvent.getExtendedKeyCodeForChar(c);
+        if (KeyEvent.CHAR_UNDEFINED == keyCode) {
+            System.err.println("Key code not found for character: " + c);
+            return;
+        }
+
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
     }
 }
