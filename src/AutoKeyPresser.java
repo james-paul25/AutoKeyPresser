70b0a1743f6459a8381239d9fb57b50d7b56b53d
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-08-08 23:17:52 +0800

    add: new feature gui is ready

--- src/AutoKeyPresser.java
+++ src/AutoKeyPresser.java
@@ -1,27 +1,24 @@
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import gui.AutoKeyPresserGUI;
+
+import javax.swing.*;
+
+import static javax.swing.UIManager.*;
 
 public class AutoKeyPresser {
 
     public static void main(String[] args) throws Exception {
-        Robot robot = new Robot();
 
-        while (true) {
-            robot.keyPress(KeyEvent.VK_3);
-            robot.keyRelease(KeyEvent.VK_3);
-            Thread.sleep(500); // wait 0.5 secon3241ds
-
-            robot.keyPress(KeyEvent.VK_2);
-            robot.keyRelease(KeyEvent.VK_2);
-            Thread.sleep(500);
-
-            robot.keyPress(KeyEvent.VK_4);
-            robot.keyRelease(KeyEvent.VK_4);
-            Thread.sleep(500);
-
-            robot.keyPress(KeyEvent.VK_1);
-            robot.keyRelease(KeyEvent.VK_1);
-            Thread.sleep(3000); // wait 3 seconds
+        try {
+            setLookAndFeel(getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        // Always run Swing GUI code on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            AutoKeyPresserGUI dialog = new AutoKeyPresserGUI();
+            dialog.setTitle("Auto Key Presser");
+            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setVisible(true);
+        });
     }
 }
